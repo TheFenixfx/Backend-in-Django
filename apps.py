96b644d3048c7441b05dcd6b335f80af26a0005f
Jhon Coder <TheFenixfx@users.noreply.github.com>
2020-08-24 13:09:05 -0400

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,10 @@
+""" Guatudu app. """
+
+# Django
+from django.apps import AppConfig
+
+class GuatuduAppConfig(AppConfig):
+    """ Guatudu app config. """
+
+    name = 'api.guatudu'
+    verbose_name = 'Guatudu'
