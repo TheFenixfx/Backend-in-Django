2f6d30af7aaeb678522067f87d6a99a26a46367c
Jhon Coder <TheFenixfx@users.noreply.github.com>
2020-08-24 13:07:44 -0400

    Add files via upload

--- business.py
+++ business.py
@@ -1,16 +1,42 @@
-"""Business view"""
+"""Business model"""
 
-# Django REST Framework
-from rest_framework import viewsets
+# Django
+from django.db import models
+from django.core.validators import RegexValidator
 
-# Serializers
-from api.guatudu.serializers import BusinessSerializer
+# Utilities
+from api.utils.models import BaseModel
 
-# Models
-from api.guatudu.models import Event
+class Business(BaseModel):
+    """ Business model. """
 
-class BusinessViewSet(viewsets.ModelViewSet):
-    """Tag viewset"""
+    name = models.CharField(max_length = 150)
+    image = models.CharField(max_length=150)
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message='Phone nubmer must be entered in the format : +999999999. Up to 15 digits allowed.'
+    )
+    phone_number = models.CharField(
+        validators=[phone_regex], 
+        max_length=17, 
+        blank=True
+    )
+    email = models.EmailField(
+        'email address',
+        unique=True,
+        error_messages={
+            'unique': 'A user with that email already exists.'
+        }
+    )
+    rating = models.IntegerField()
+    description = models.CharField(max_length=250)
 
-    queryset = Event.objects.all()
-    serializer_class = BusinessSerializer
+    # admin_profiles = models.ManyToManyField('guatudu.AdminProfile')
+    
+    def __str__(self):
+        """ Return business string representation"""
+        return str(self.name)
+    
+    class Meta:
+        db_table = 'business'
+
